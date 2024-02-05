68dbdfce1e27b55fb85477c14b2104f8e6f4e64f
WissamRh <134094610+WissamRh@users.noreply.github.com>
2024-02-05 19:18:02 +0200

    Update init.sql
    sql updste

--- init.sql
+++ init.sql
@@ -4,8 +4,9 @@ USE mydatabase;
 -- Your SQL initialization script goes here
 CREATE TABLE mytable (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
+    name VARCHAR(255) NOT NULL,
+    phone INT NOT NULL
 );
 
 -- Insert some sample data
-INSERT INTO mytable (name) VALUES ('Example 1'), ('Example 2');
+INSERT INTO mytable (name, phone) VALUES ('Example 1', '11111'), ('Example 2', '222222');
