5ffae2cd4bd45f64279a20030cdc80d1579046e1
WissamRh <134094610+WissamRh@users.noreply.github.com>
2024-02-07 13:56:07 +0200

    Update index.php

--- index.php
+++ index.php
@@ -12,10 +12,19 @@
         <label for="name">Name:</label>
         <input type="text" id="name" name="name" required><br><br>
 
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" required><br><br>
+        <label for="phonr">Phone:</label>
+        <input type="phone" id="phone" name="phone" required><br><br>
 
         <button type="submit">Submit</button>
     </form>
+
+
+    <button id="redirectButton">Go to Second Page</button>
+
+    <script>
+        document.getElementById("redirectButton").addEventListener("click", function() {
+            window.location.href = "second.html";
+        });
+    </script>
 </body>
 </html>
